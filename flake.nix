033428638a756474a32b3f104d24cd459c14b4d8
Logan Crandall <crandall.logan@gmail.com>
2024-09-04 18:45:21 -0400

    Assign string to .url

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-generators = "github.com:nix-community/nixos-generators";
+    nixos-generators.url = "github.com:nix-community/nixos-generators";
   };
 
   outputs = { self, nixpkgs, ... }@attrs: {
